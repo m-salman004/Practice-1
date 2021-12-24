116dc25fa097465b71acfc117e855d5fea1dd13d
Muhammad Salman <92182319+m-salman004@users.noreply.github.com>
2021-12-23 22:13:03 -0800

    Show Variables

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ document.getElementById("alertName").onclick = function() {
     orignalstmnt.innerHTML = "alert(" + "'" + val + "'" + ")";
     outputs.innerHTML = "Your Input Value is " + val;
 }
+// Show Variables
+document.getElementById("showVariablename").onclick = function() {
+    var varname = "<li>A variable can't contain any spaces.</li><li>A variable name contain only letters,numbers,dollor signs and underscores.</li><li>Though a variable name can't be any of javascript's keywords.It can keywords.</li>"
+    outputs.style.textAlign = "left";
+    outputs.style.paddingLeft = "50px";
+    outputs.innerHTML = varname;
+}
